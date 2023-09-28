45a6a105f7ede6a1a793ff1e7a369c707ac1a538
alexbolog <mbolog.alex@gmail.com>
2023-09-28 10:48:40 +0300

    added initial specs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+// Specs
+// Admin side
+// - Create a loan: project id, crowdfunding start/end and target etc
+// - cancel crowdfund and return funds (maybe)
+// - approve crowdfund and proceed with funding
+// - distribute repayments to users
+// Bot side
+// - send loan shares to users
+// - distribute repayments to users
+// User side
+// - invest in a loan
+// - withdraw from investing in a loan (MiCa compliance)
+// - claim loan repayments
+// - claim unsuccessful crowdfunding funds back
+
 #![no_std]
 
 multiversx_sc::imports!();
