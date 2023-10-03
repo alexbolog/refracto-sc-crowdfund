638a7622650a1abc05949b476ed0a76c971b7278
alexbolog <mbolog.alex@gmail.com>
2023-10-03 14:42:13 +0300

    test progress

---
+++ tests/invest/restrictions.rs
@@ -0,0 +1,114 @@
+use loan_crowdfund_sc::{
+    constants::ERR_CANNOT_INVEST_IN_CRT_STATE, types::crowdfunding_state::ProjectFundingState,
+};
+
+use crate::test_state::{LoanCfTestState, INVESTOR_1_ADDRESS_EXPR};
+
+#[test]
+fn failed_invest_during_funding_state_pending() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_valid_mockup_project();
+
+    state.check_funding_state(1, ProjectFundingState::Pending);
+    state.invest_and_expect_err(
+        INVESTOR_1_ADDRESS_EXPR,
+        100,
+        1,
+        ERR_CANNOT_INVEST_IN_CRT_STATE,
+    );
+}
+
+#[test]
+fn failed_invest_during_funding_state_successful() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_valid_mockup_project();
+    state.set_block_timestamp("101");
+    state.invest(INVESTOR_1_ADDRESS_EXPR, 90001, 1);
+    state.set_block_timestamp("10001");
+
+    state.check_funding_state(1, ProjectFundingState::CFSuccessful);
+
+    state.invest_and_expect_err(
+        INVESTOR_1_ADDRESS_EXPR,
+        100,
+        1,
+        ERR_CANNOT_INVEST_IN_CRT_STATE,
+    );
+}
+
+#[test]
+fn failed_invest_during_funding_state_failed() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_valid_mockup_project();
+    state.set_block_timestamp("10001");
+
+    state.check_funding_state(1, ProjectFundingState::CFFailed);
+
+    state.invest_and_expect_err(
+        INVESTOR_1_ADDRESS_EXPR,
+        100,
+        1,
+        ERR_CANNOT_INVEST_IN_CRT_STATE,
+    );
+}
+
+#[test]
+fn failed_invest_during_funding_state_cancelled() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_valid_mockup_project();
+    state.cancel_project(1);
+
+    state.invest_and_expect_err(
+        INVESTOR_1_ADDRESS_EXPR,
+        100,
+        1,
+        ERR_CANNOT_INVEST_IN_CRT_STATE,
+    );
+}
+
+#[test]
+fn failed_invest_during_funding_state_loan_active() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_valid_mockup_project();
+    state.set_block_timestamp("101");
+    state.invest(INVESTOR_1_ADDRESS_EXPR, 90001, 1);
+    state.set_block_timestamp("10001");
+    state.claim(INVESTOR_1_ADDRESS_EXPR, 1, 1);
+
+    state.check_funding_state(1, ProjectFundingState::LoanActive);
+
+    state.invest_and_expect_err(
+        INVESTOR_1_ADDRESS_EXPR,
+        100,
+        1,
+        ERR_CANNOT_INVEST_IN_CRT_STATE,
+    );
+}
+
+#[test]
+fn failed_invest_during_funding_state_completed() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_valid_mockup_project();
+    state.set_block_timestamp("101");
+    state.invest(INVESTOR_1_ADDRESS_EXPR, 90001, 1);
+    state.set_block_timestamp("10001");
+    state.claim_loan_funds(1);
+
+    state.repay_loan(1, 11000);
+    state.admin_distribute_repayment(1);
+
+    state.check_funding_state(1, ProjectFundingState::Completed);
+
+    state.invest_and_expect_err(
+        INVESTOR_1_ADDRESS_EXPR,
+        100,
+        1,
+        ERR_CANNOT_INVEST_IN_CRT_STATE,
+    );
+}
