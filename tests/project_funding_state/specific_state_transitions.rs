433654c2e65915f05c8e58e58350965f04fc7ba0
alexbolog <mbolog.alex@gmail.com>
2023-10-03 13:46:01 +0300

    divided state transition tests into scoped modules

---
+++ tests/project_funding_state/specific_state_transitions.rs
@@ -0,0 +1,18 @@
+use crate::test_state::{LoanCfTestState, INVESTOR_1_ADDRESS_EXPR};
+use loan_crowdfund_sc::types::crowdfunding_state::ProjectFundingState;
+
+#[test]
+fn funding_state_cool_off_to_failed() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_valid_mockup_project();
+    state.set_block_timestamp("101");
+    state.invest(INVESTOR_1_ADDRESS_EXPR, 90001);
+    state.set_block_timestamp("102");
+
+    state.check_funding_state(1, ProjectFundingState::CFWaitingCooloff);
+    state.withdraw(INVESTOR_1_ADDRESS_EXPR, 1, 90001);
+
+    state.set_block_timestamp("10001");
+    state.check_funding_state(1, ProjectFundingState::CFFailed);
+}
