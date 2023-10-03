f8f607a22e5e91a991f2aaab36285a5d16ef7d9c
alexbolog <mbolog.alex@gmail.com>
2023-10-03 12:02:04 +0300

    added endpoint definitions

--- src/admin.rs
+++ src/admin.rs
@@ -2,5 +2,12 @@ multiversx_sc::imports!();
 
 #[multiversx_sc::module]
 pub trait AdminModule: crate::permissions::PermissionsModule {
-    
+    #[endpoint(create)]
+    fn create_project(&self) {}
+
+    #[endpoint(cancel)]
+    fn cancel_project(&self) {}
+
+    #[endpoint(adminDistributeRepayment)]
+    fn admin_distribute_repayments(&self) {}
 }
--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,13 @@
 multiversx_sc::imports!();
 
 #[multiversx_sc::module]
-pub trait CommonModule {}
+pub trait CommonModule {
+    #[view(getExpectedInterest)]
+    fn get_expected_interest(&self) {}
+
+    #[view(getExpectedLateFees)]
+    fn get_expected_late_fees(&self) {}
+
+    #[view(getFinalAmount)]
+    fn get_final_amount(&self) {}
+}
