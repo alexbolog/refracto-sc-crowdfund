9bbe549ada6326abdbc4416ff384593b38fbc29a
alexbolog <mbolog.alex@gmail.com>
2023-10-03 15:03:03 +0300

    params + constants

--- src/common.rs
+++ src/common.rs
@@ -5,13 +5,19 @@ multiversx_sc::imports!();
 #[multiversx_sc::module]
 pub trait CommonModule {
     #[view(getExpectedInterest)]
-    fn get_expected_interest(&self) {}
+    fn get_expected_interest(&self, project_id: u64) -> BigUint {
+        todo!()
+    }
 
     #[view(getExpectedLateFees)]
-    fn get_expected_late_fees(&self) {}
+    fn get_expected_late_fees(&self, project_id: u64) -> BigUint {
+        todo!()
+    }
 
-    #[view(getFinalAmount)]
-    fn get_final_amount(&self) {}
+    #[view(getTotalAmount)]
+    fn get_total_amount(&self, project_id: u64) -> BigUint {
+        todo!()
+    }
 
     #[view(getFundingState)]
     fn get_funding_state(&self, project_id: u64) -> ProjectFundingState {
