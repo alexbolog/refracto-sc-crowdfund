f3beda77756ea85ad061b17b0ac964856ac49e37
alexbolog <mbolog.alex@gmail.com>
2023-10-03 14:42:08 +0300

    added more constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,4 @@
 pub const COOL_OFF_PERIOD: u64 = 3600 * 24 * 14; // 14 days
 
 pub const ERR_COOL_OFF_EXPIRED: &str = "Cannot withdraw, cool off period expired";
+pub const ERR_CANNOT_INVEST_IN_CRT_STATE: &str = "Cannot invest in current funding state";
