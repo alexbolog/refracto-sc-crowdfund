5e58dd93713e957816d8fc092d1aae00e4eb3766
alexbolog <mbolog.alex@gmail.com>
2023-10-09 13:05:27 +0300

    added addAdmin/removeAdmin endpoints; added owner check on admin address in case the owner is not an admin

--- loan-crowdfund-sc/src/permissions.rs
+++ loan-crowdfund-sc/src/permissions.rs
@@ -2,8 +2,26 @@ multiversx_sc::imports!();
 
 #[multiversx_sc::module]
 pub trait PermissionsModule {
+    #[endpoint(addAdmins)]
+    fn add_admins(&self, addresses: MultiValueManagedVec<ManagedAddress>) {
+        self.require_caller_is_admin();
+
+        for address in addresses.iter() {
+            self.admin_list().insert(address.clone_value());
+        }
+    }
+
+    #[endpoint(removeAdmins)]
+    fn remove_admins(&self, addresses: MultiValueManagedVec<ManagedAddress>) {
+        self.require_caller_is_admin();
+
+        for address in addresses.iter() {
+            self.admin_list().remove(&address);
+        }
+    }
+
     fn is_address_admin(&self, address: &ManagedAddress) -> bool {
-        self.admin_list().contains(address)
+        self.admin_list().contains(address) || address == &self.blockchain().get_caller()
     }
 
     fn require_caller_is_admin(&self) {
