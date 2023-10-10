4ff99daf06abc41157a089aecb8c7d165824787b
alexbolog <mbolog.alex@gmail.com>
2023-10-10 13:17:33 +0300

    added cool off test; changed get_recorded_payment to retrieve the oldest matching record by default

--- loan-crowdfund-sc/tests/invest/cool_off_withdrawal.rs
+++ loan-crowdfund-sc/tests/invest/cool_off_withdrawal.rs
@@ -1,4 +1,6 @@
-use loan_crowdfund_sc::constants::{ERR_CANNOT_WITHDRAW_IN_CRT_STATE, ERR_COOL_OFF_EXPIRED};
+use loan_crowdfund_sc::constants::{
+    ERR_CANNOT_WITHDRAW_IN_CRT_STATE, ERR_COOL_OFF_EXPIRED, ERR_INVESTMENT_NOT_FOUND,
+};
 
 use crate::test_state::{LoanCfTestState, ACCOUNT_BALANCE_EXPR, INVESTOR_1_ADDRESS_EXPR};
 
@@ -26,10 +28,12 @@ fn successful_cool_off_withdrawal_with_multiple_investments() {
     state.whitelist_address(INVESTOR_1_ADDRESS_EXPR);
     state.set_block_timestamp(101);
     state.invest(INVESTOR_1_ADDRESS_EXPR, 1000, 1);
-    state.invest(INVESTOR_1_ADDRESS_EXPR, 1000, 1);
     state.set_block_timestamp(102);
+    state.invest(INVESTOR_1_ADDRESS_EXPR, 1000, 1);
+    state.set_block_timestamp(103);
 
-    state.withdraw(INVESTOR_1_ADDRESS_EXPR, 1, 2000);
+    state.withdraw(INVESTOR_1_ADDRESS_EXPR, 1, 1000);
+    state.withdraw(INVESTOR_1_ADDRESS_EXPR, 1, 1000);
 
     state.check_investor_share_balance(INVESTOR_1_ADDRESS_EXPR, "1", "0");
     state.check_investor_usdc_balance(INVESTOR_1_ADDRESS_EXPR, ACCOUNT_BALANCE_EXPR);
@@ -71,4 +75,18 @@ fn failed_withdrawal_after_cool_off_state() {
         1000,
         ERR_CANNOT_WITHDRAW_IN_CRT_STATE,
     );
+}
+
+#[test]
+fn failed_withdrawal_with_merged_investments() {
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_fully_mocked_project();
+    state.whitelist_address(INVESTOR_1_ADDRESS_EXPR);
+    state.set_block_timestamp(101);
+    state.invest(INVESTOR_1_ADDRESS_EXPR, 1000, 1);
+    state.invest(INVESTOR_1_ADDRESS_EXPR, 1000, 1);
+    state.set_block_timestamp(102);
+
+    state.withdraw_and_expect_err(INVESTOR_1_ADDRESS_EXPR, 1, 2000, ERR_INVESTMENT_NOT_FOUND);
 }
