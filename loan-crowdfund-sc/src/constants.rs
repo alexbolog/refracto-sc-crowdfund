9a45208d6d372e19ae202ba95a3b5fdfc35fe32a
alexbolog <mbolog.alex@gmail.com>
2023-10-10 12:00:56 +0300

    cool off withdrawal initial implementation

--- loan-crowdfund-sc/src/constants.rs
+++ loan-crowdfund-sc/src/constants.rs
@@ -6,3 +6,7 @@ pub const ERR_CANNOT_OVER_FINANCE: &str = "Investment exceeds project goal";
 pub const ERR_KYC_NOT_DONE: &str = "KYC not verified";
 pub const ERR_INVALID_PROJECT_ID: &str = "Invalid project id";
 pub const ERR_INVALID_PAYMENT_TOKEN: &str = "Invalid payment token";
+pub const ERR_INVALID_PAYMENT_NONCE: &str = "Invalid payment nonce";
+pub const ERR_INVESTMENT_NOT_FOUND: &str = "Investment not found";
+pub const ERR_CANNOT_WITHDRAW_IN_CRT_STATE: &str = "Cannot withdraw in current funding state";
+pub const ERR_WITHDRAW_EXPIRED: &str = "Cannot withdraw, cool off period expired";
--- loan-crowdfund-sc/src/lib.rs
+++ loan-crowdfund-sc/src/lib.rs
@@ -30,7 +30,9 @@ use types::crowdfunding_state::CrowdfundingStateContext;
 
 use crate::{
     constants::{
-        ERR_CANNOT_INVEST_IN_CRT_STATE, ERR_INVALID_PAYMENT_TOKEN, ERR_INVALID_PROJECT_ID,
+        COOL_OFF_PERIOD, ERR_CANNOT_INVEST_IN_CRT_STATE, ERR_CANNOT_WITHDRAW_IN_CRT_STATE,
+        ERR_INVALID_PAYMENT_NONCE, ERR_INVALID_PAYMENT_TOKEN, ERR_INVALID_PROJECT_ID,
+        ERR_INVESTMENT_NOT_FOUND, ERR_WITHDRAW_EXPIRED,
     },
     types::crowdfunding_state::ProjectFundingState,
 };
@@ -83,8 +85,31 @@ pub trait LoanCrowdfundScContract:
     #[payable("*")]
     #[endpoint(withdraw)]
     fn withdraw(&self) {
+        let caller = self.blockchain().get_caller();
         let payment = self.get_withdraw_payment_or_fail_if_invalid();
-        
+        let mut cf_state = self.get_project_by_nonce_or_fail(payment.token_nonce);
+
+        let investment_amount = self.get_investment_amount_by_shares_amount(
+            &payment.amount,
+            &cf_state.share_price_per_unit,
+        );
+        let recorded_payment =
+            self.get_recorded_payment(cf_state.project_id, &caller, &investment_amount);
+
+        self.require_withdraw_is_possible(&cf_state, recorded_payment.1);
+
+        self.update_successful_cool_off_withdrawal(
+            &mut cf_state,
+            &investment_amount,
+            recorded_payment,
+        );
+
+        self.send().direct_esdt(
+            &caller,
+            &cf_state.project_payment_token,
+            0,
+            &investment_amount,
+        );
     }
 
     #[endpoint(claim)]
@@ -119,11 +144,54 @@ pub trait LoanCrowdfundScContract:
         self.crowdfunding_state(state.project_id).set(state);
     }
 
+    fn update_successful_cool_off_withdrawal(
+        &self,
+        state: &mut CrowdfundingStateContext<Self::Api>,
+        amount: &BigUint,
+        recorded_payment: (ManagedAddress, u64, BigUint),
+    ) {
+        state.cf_progress -= amount;
+        self.recorded_payments(state.project_id)
+            .remove(&recorded_payment);
+        self.crowdfunding_state(state.project_id).set(state);
+    }
+
+    fn get_investment_amount_by_shares_amount(
+        &self,
+        shares_amount: &BigUint,
+        price_per_share: &BigUint,
+    ) -> BigUint {
+        shares_amount * price_per_share
+    }
+
+    fn get_recorded_payment(
+        &self,
+        project_id: u64,
+        caller: &ManagedAddress,
+        investment_token_amount: &BigUint,
+    ) -> (ManagedAddress, u64, BigUint) {
+        for recorded_payment in self.recorded_payments(project_id).iter() {
+            let (address, timestamp, recorded_amount) = recorded_payment;
+            if &address == caller && &recorded_amount == investment_token_amount {
+                return (address, timestamp, recorded_amount);
+            }
+        }
+        sc_panic!(ERR_INVESTMENT_NOT_FOUND);
+    }
+
     fn get_project_by_id_or_fail(&self, project_id: u64) -> CrowdfundingStateContext<Self::Api> {
         let state_strg = self.crowdfunding_state(project_id);
         require!(!state_strg.is_empty(), ERR_INVALID_PROJECT_ID);
 
         state_strg.get()
+    }
+
+    fn get_project_by_nonce_or_fail(&self, nonce: u64) -> CrowdfundingStateContext<Self::Api> {
+        let nonce_mapping_strg = self.project_id_by_loan_share_nonce(nonce);
+        require!(!nonce_mapping_strg.is_empty(), ERR_INVALID_PAYMENT_NONCE);
+
+        let project_id = nonce_mapping_strg.get();
+        self.get_project_by_id_or_fail(project_id)
     }
 
     fn get_invest_payment_or_fail_if_invalid(
@@ -160,5 +228,26 @@ pub trait LoanCrowdfundScContract:
             &state == &ProjectFundingState::CFActive,
             ERR_CANNOT_INVEST_IN_CRT_STATE
         );
+    }
+
+    fn require_withdraw_is_possible(
+        &self,
+        cf_state: &CrowdfundingStateContext<Self::Api>,
+        investment_timestamp: u64,
+    ) {
+        let block_timestamp = self.blockchain().get_block_timestamp();
+        let state = cf_state.get_funding_state(
+            &self.get_aggregated_cool_off_amount(cf_state.project_id),
+            block_timestamp,
+        );
+        require!(
+            state == ProjectFundingState::CFActive
+                || state == ProjectFundingState::CFWaitingCooloff,
+            ERR_CANNOT_WITHDRAW_IN_CRT_STATE
+        );
+        require!(
+            investment_timestamp + COOL_OFF_PERIOD < block_timestamp,
+            ERR_WITHDRAW_EXPIRED
+        )
     }
 }
