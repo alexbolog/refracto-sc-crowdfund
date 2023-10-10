7f6286757f6ae91ec3a504e7c747bcc29cfb63f5
alexbolog <mbolog.alex@gmail.com>
2023-10-10 18:52:30 +0300

    added mock implementation for claiming funds

--- loan-crowdfund-sc/src/beneficiary.rs
+++ loan-crowdfund-sc/src/beneficiary.rs
@@ -1,7 +1,14 @@
 multiversx_sc::imports!();
 
 #[multiversx_sc::module]
-pub trait BeneficiaryModule: crate::permissions::PermissionsModule {
+pub trait BeneficiaryModule:
+    crate::permissions::PermissionsModule + crate::storage::config::ConfigModule
+{
     #[endpoint(claimLoanFunds)]
-    fn claim_loan_funds(&self, project_id: u64) {}
+    fn claim_loan_funds(&self, project_id: u64) {
+        let mut state = self.crowdfunding_state(project_id).get();
+        state.loan_start_timestamp = self.blockchain().get_block_timestamp();
+        state.is_loan_active = true;
+        self.crowdfunding_state(project_id).set(&state);
+    }
 }
