d6a3a81c8737840297ac13e37d28eee34e2b8ad8
alexbolog <mbolog.alex@gmail.com>
2023-10-16 14:26:25 +0300

    removed comments

--- loan-crowdfund-sc/src/lib.rs
+++ loan-crowdfund-sc/src/lib.rs
@@ -1,29 +1,3 @@
-// Specs
-// Admin side
-// - Create a loan: project id, crowdfunding start/end and target etc
-// - cancel crowdfund and return funds (maybe)
-// - approve crowdfund and proceed with funding -> might not be needed, if we make the developer claim the funds
-// - distribute repayments to users
-
-// Bot side
-// - send loan shares to users
-// - distribute repayments to users
-
-// User side
-// - invest in a loan
-// - withdraw from investing in a loan (MiCa compliance)
-// - claim loan repayments
-// - claim unsuccessful crowdfunding funds back
-
-// Beneficiary side (developer)
-// - claim funds from successful crowdfunding
-// - repay loan
-// - interest rate calculator + penalty fee calculator
-
-// Add expected repayment amount check in repayment distribution
-
-// !!! Single, full claiming + interest
-
 #![no_std]
 
 use types::crowdfunding_state::CrowdfundingStateContext;
