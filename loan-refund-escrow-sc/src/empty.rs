300d1974b31b05396eb1be15b090b91c793a3462
alexbolog <mbolog.alex@gmail.com>
2023-10-16 10:41:16 +0300

    implemented escrow logic

--- loan-refund-escrow-sc/src/empty.rs
+++ loan-refund-escrow-sc/src/empty.rs
@@ -2,7 +2,6 @@
 
 multiversx_sc::imports!();
 
-/// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
 pub trait LoanRefundEscrowScContract {
     #[init]
@@ -19,10 +18,51 @@ pub trait LoanRefundEscrowScContract {
             .set_if_empty(loan_repayment_token_id);
     }
 
-    // see if needed
-    // #[payable("*")]
-    // #[endpoint(depositRepayment)]
-    // fn deposit_repayment(&self) { }
+    #[payable("*")]
+    #[endpoint(depositLoanRepayment)]
+    fn deposit_loan_repayment(&self) {
+        let caller = self.blockchain().get_caller();
+        let payment = self.call_value().single_esdt();
+
+        self.require_address_is_developer_or_admin(&caller);
+        self.require_payment_token_is_loan_repayment_token(&payment.token_identifier);
+    }
+
+    #[endpoint(withdrawRepaymentFunds)]
+    fn withdraw_repayment_funds(&self) -> BigUint {
+        let caller = self.blockchain().get_caller();
+        self.require_address_is_developer_or_admin(&caller);
+
+        let token_id = self.loan_repayment_token_id().get();
+        let balance = self
+            .blockchain()
+            .get_sc_balance(&EgldOrEsdtTokenIdentifier::esdt(token_id.clone()), 0);
+        self.send().direct_esdt(&caller, &token_id, 0, &balance);
+
+        balance
+    }
+
+    fn require_payment_token_is_loan_repayment_token(&self, token_identifier: &TokenIdentifier) {
+        require!(
+            token_identifier == &self.loan_repayment_token_id().get(),
+            "Invalid token identifier used for deposit"
+        );
+    }
+
+    fn require_address_is_developer_or_admin(&self, address: &ManagedAddress) {
+        let is_admin: bool = self
+            .cf_contract_proxy(self.blockchain().get_owner_address())
+            .is_address_admin(address.clone())
+            .execute_on_dest_context();
+        require!(
+            is_admin || address == &self.developer_wallet_address().get(),
+            "Only project developer/admins can call this function"
+        );
+    }
+
+    #[proxy]
+    fn cf_contract_proxy(&self, sc_address: ManagedAddress)
+        -> crowdfund_sc_proxy::Proxy<Self::Api>;
 
     #[view(getProjectId)]
     #[storage_mapper("project_id")]
@@ -35,4 +75,19 @@ pub trait LoanRefundEscrowScContract {
     #[view(getLoanRepaymentTokenId)]
     #[storage_mapper("loan_repayment_token_id")]
     fn loan_repayment_token_id(&self) -> SingleValueMapper<TokenIdentifier>;
+}
+
+/*
+#[view(getIsAddressAdmin)]
+    fn is_address_admin(&self, address: &ManagedAddress) -> bool
+ */
+
+mod crowdfund_sc_proxy {
+    multiversx_sc::imports!();
+
+    #[multiversx_sc::proxy]
+    pub trait CrowdfundSc {
+        #[view(getIsAddressAdmin)]
+        fn is_address_admin(&self, address: &ManagedAddress) -> bool;
+    }
 }
