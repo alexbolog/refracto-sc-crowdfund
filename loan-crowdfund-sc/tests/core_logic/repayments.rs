c6d94a5ba0f40a103de5bef195e5453fcf52f0fb
alexbolog <mbolog.alex@gmail.com>
2023-12-13 12:14:25 +0200

    added claim tests

--- loan-crowdfund-sc/tests/core_logic/repayments.rs
+++ loan-crowdfund-sc/tests/core_logic/repayments.rs
@@ -4,7 +4,7 @@ use loan_crowdfund_sc::types::crowdfunding_state::{
 
 use crate::test_state::{
     mockups::{MOCKUP_CF_DEFAULT_COVER_MIN_PRINCIPAL, MOCKUP_CF_DEFAULT_COVER_MIN_REPAYMENT},
-    LoanCfTestState, ACCOUNT_BALANCE_EXPR, INVESTOR_1_ADDRESS_EXPR,
+    LoanCfTestState,
 };
 
 #[test]
@@ -30,26 +30,4 @@ fn repayment_rate_computed_correctly() {
         * INTEREST_RATE_DENOMINATION
         / MOCKUP_CF_DEFAULT_COVER_MIN_PRINCIPAL;
     state.check_repayment_rate(project_id, expected_repayment_rate);
-}
-
-#[test]
-fn claim_rewards_yields_correct_amount() {
-    let project_id = 1;
-    let mut state = LoanCfTestState::new();
-    state.deploy_contract();
-    state.create_default_mockup_in_state(project_id, &ProjectFundingState::Completed);
-
-    state.claim(
-        INVESTOR_1_ADDRESS_EXPR,
-        1,
-        MOCKUP_CF_DEFAULT_COVER_MIN_PRINCIPAL,
-    );
-
-    let initial_balance: u64 = ACCOUNT_BALANCE_EXPR
-        .parse()
-        .expect("Failed to parse string to u64");
-    let expected_balance = initial_balance - MOCKUP_CF_DEFAULT_COVER_MIN_PRINCIPAL
-        + MOCKUP_CF_DEFAULT_COVER_MIN_REPAYMENT;
-
-    state.check_address_usdc_balance(INVESTOR_1_ADDRESS_EXPR, &expected_balance.to_string());
 }
