291647f2beb018d19dec27cec60df6c555162cc2
alexbolog <mbolog.alex@gmail.com>
2023-12-13 11:38:47 +0200

    added more tests - repayment, reward rate and claim reward

---
+++ loan-crowdfund-sc/tests/core_logic/repayments.rs
@@ -0,0 +1,47 @@
+use loan_crowdfund_sc::types::crowdfunding_state::{
+    ProjectFundingState, INTEREST_RATE_DENOMINATION,
+};
+
+use crate::test_state::{
+    mockups::{MOCKUP_CF_DEFAULT_COVER_MIN_PRINCIPAL, MOCKUP_CF_DEFAULT_COVER_MIN_REPAYMENT},
+    LoanCfTestState, INVESTOR_1_ADDRESS_EXPR,
+};
+
+#[test]
+fn repayment_value_computed_correctly() {
+    let project_id = 1;
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_default_mockup_in_state(project_id, &ProjectFundingState::Completed);
+
+    state.check_funding_state(project_id, ProjectFundingState::Completed);
+
+    state.check_total_repayment_amount(project_id, MOCKUP_CF_DEFAULT_COVER_MIN_REPAYMENT);
+}
+
+#[test]
+fn repayment_rate_computed_correctly() {
+    let project_id = 1;
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_default_mockup_in_state(project_id, &ProjectFundingState::Completed);
+
+    let expected_repayment_rate = MOCKUP_CF_DEFAULT_COVER_MIN_REPAYMENT
+        * INTEREST_RATE_DENOMINATION
+        / MOCKUP_CF_DEFAULT_COVER_MIN_PRINCIPAL;
+    state.check_repayment_rate(project_id, expected_repayment_rate);
+}
+
+#[test]
+fn claim_rewards_yields_correct_amount() {
+    let project_id = 1;
+    let mut state = LoanCfTestState::new();
+    state.deploy_contract();
+    state.create_default_mockup_in_state(project_id, &ProjectFundingState::Completed);
+
+    state.claim(
+        INVESTOR_1_ADDRESS_EXPR,
+        1,
+        MOCKUP_CF_DEFAULT_COVER_MIN_PRINCIPAL,
+    );
+}
